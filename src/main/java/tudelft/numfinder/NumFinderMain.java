cacedf63db79cc2b66b56743f2bf1516edc826f4
Isaac Mecchi <isaacmecchi@gmail.com>
2021-01-19 12:43:11 -0300

    Segunda aula ajustando testes.

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -9,7 +9,8 @@ public class NumFinderMain {
         // nf.find(new int[] {4, 25, 7, 9});
 
         // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+//        nf.find(new int[] {4, 3, 2, 1});
+        nf.find(new int[] {5,4,3,2,1,Integer.MAX_VALUE, 9,6,7,8, Integer.MIN_VALUE, 0,1,2,3});
 
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
