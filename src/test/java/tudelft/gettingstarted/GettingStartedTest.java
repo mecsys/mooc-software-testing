c447ab6ee74b3d1c357194e54386f38add3fef24
Isaac Mecchi <isaacmecchi@gmail.com>
2021-10-16 15:41:07 -0300

    Update GettingStartedTest.java
    addFiveToMinuz20
    
    Add 5 + (-20)

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -19,9 +19,9 @@ public class GettingStartedTest {
         Assertions.assertEquals(5, result);
     }
 
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
 }
