62a2c84d9de86462f62da4dbeef0d1c40c9fb075
012489631 <012489631@LAPTOP-PPDGOT2D>
2021-10-25 15:44:32 -0300

    Criacao de testes de persistencia de objetos com conseulta a banco SQL

--- src/test/java/tudelft/invoice/InvoiceFilterTest.java
+++ src/test/java/tudelft/invoice/InvoiceFilterTest.java
@@ -5,24 +5,26 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-//public class InvoiceFilterTest {
-//    @Test
-//    void filterInvoices() {
-//
-//        InvoiceDao dao = new InvoiceDao();
-//        Invoice mauricio = new Invoice("Mauricio", 20.0);
-//        Invoice arie = new Invoice("Arie", 300.0);
-//
-//        dao.save(mauricio);
-//        dao.save(arie);
-//
-//        InvoiceFilter filter = new InvoiceFilter();
-//        List<Invoice> result = filter.filter();
-//
-//        Assertions.assertEquals(mauricio, result.get(0));
-//        Assertions.assertEquals(1, result.size());
-//
-//        dao.close();
-//    }
-//
-//}
+public class InvoiceFilterTest {
+    @Test
+    void filterInvoices() {
+
+        InvoiceDao dao = new InvoiceDao();
+        Invoice mauricio = new Invoice("Mauricio", 20.0);
+        Invoice arie = new Invoice("Arie", 300.0);
+        Invoice laura = new Invoice("Laura", 50.0);
+
+        dao.save(mauricio);
+        dao.save(arie);
+        dao.save(laura); 
+
+        InvoiceFilter filter = new InvoiceFilter();
+        List<Invoice> result = filter.filter();
+
+        Assertions.assertEquals(mauricio, result.get(0));
+        Assertions.assertEquals(2, result.size());
+
+        dao.close();
+    }
+
+}
